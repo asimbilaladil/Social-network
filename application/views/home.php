5dd0be11e51eda68bd399c50cf5877455c457716
asimbilaladil <asimbilal@Asims-MacBook.local>
2016-06-26 20:49:02 +0500

    add post page

---
+++ application/views/home.php
@@ -0,0 +1,140 @@
+   <!-- PROFILE FEATURE -->
+    <section class="profile-feature">
+        <div class="awe-parallax bg-profile-feature"></div>
+        <div class="awe-overlay overlay-color-3"></div>
+        <div class="container">
+
+   
+
+
+            <div class="info-author">
+                <div class="image">
+                    <img src="<?php echo 'https://graph.facebook.com/'. $user_profile['id'] .'/picture?type=large' ?>" alt="">
+                </div>    
+                <div class="name-author">
+                    <h2 class="big"> <?php echo $user_profile['name']; ?> </h2>
+                </div>     
+                <div class="address-author">
+                    <i class="fa fa-map-marker"></i>
+                    <h3>  <?php if(array_key_exists('location', $user_profile)) { echo $user_profile['location']['name']; }  ?> </h3>
+                </div>
+                <div class="address-author">
+                    <i class="fa fa-sign-out"></i>
+                   <h3> <a class="text-color" href="<?php echo site_url('login/logout') ?>"> Logout </a></h3>
+                </div>
+
+            </div>
+            <div class="info-follow">
+                <div class="trophies">
+                    <span style="color: greenyellow">$ 12</span>
+                    <p style="color: greenyellow" class="buyer">Sales Amount</p>
+                </div>
+                <div class="trophies" >
+                    <span style="color: red">$ 12</span>
+                    <p style="color: red">Buyers Amount</p>
+                </div>
+                <div class="trophies" >
+                    <span style="color: cornflowerblue">$ 20</span>
+                    <p style="color: cornflowerblue">Marketing Amount</p>
+                </div>
+                <div class="trophies">
+                    <span>$ 149,902</span>
+                    <p>Total</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- END / PROFILE FEATURE -->
+    <section id="after-slider" class="after-slider section">
+        <div class="awe-color bg-color-1"></div>
+        <div class="after-slider-bg-2"></div>
+        <div class="container">
+    
+            <div class="after-slider-content tb">
+                <div class="inner tb-cell">
+                    <h4>What did you do today?</h4>
+                    <div class="course-keyword">
+                        <input type="text" placeholder="Amount">
+                    </div>
+                  
+                   <div class="mc-select-wrap">
+                        <div class="mc-select">
+                            <select class="select" name="" id="all-categories" style="z-index: 10; opacity: 0;">
+                                <option value="" selected="">Sales</option>
+                                <option value="">Buyers</option>
+                                  <option value="">Marketing</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="tb-cell text-right">
+                    <div class="form-actions">
+                        <input type="submit" value="Post" class="mc-btn btn-style-1" style=" width: 70%; margin-bottom: 10px;">                    </div>
+    
+                </div>
+
+            </div>
+
+    
+        </div>
+    </section>
+    <section class="blog">
+
+        <div class="container">
+            <div class="row">
+
+                <!-- BLOG LIST -->
+                <div class="col-md-12">
+                    <div class="blog-single-content">
+                        <!-- POST -->
+                        <div class="post post-single">
+                     
+
+
+                            <!-- ABOUT AUTHOR -->
+                            <div class="about-author">
+                                <div class="image-thumb fl">
+                                </div>
+                                <div class="author-info">
+                                    <h6 class="author-title"><i  class="fa fa-calendar"> 26/06/2016</i> </h6>
+                                    <span class="author-name">Sales $ 100000</span>
+                                                         
+                                </div>
+                            </div>
+                            <!-- END / ABOUT AUTHOR -->
+
+                        </div>
+                        <!-- END / POST -->
+ <!-- POST -->
+                        <div class="post post-single">
+                     
+
+
+                            <!-- ABOUT AUTHOR -->
+                            <div class="about-author">
+                                <div class="image-thumb fl">
+                                </div>
+                                <div class="author-info">
+                                    <h6 class="author-title"><i  class="fa fa-calendar"> 26/06/2016</i> </h6>
+                                    <span class="author-name">Buyers $ 100000</span>
+                                                         
+                                </div>
+                            </div>
+                            <!-- END / ABOUT AUTHOR -->
+                            
+                        </div>
+                        <!-- END / POST -->                        
+
+
+                        
+                    </div>
+                </div>
+                <!-- END / BLOG LIST -->
+
+
+
+            </div>
+        </div>
+
+    </section>
+
